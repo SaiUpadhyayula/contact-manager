094ab4f5270c5a4d9b89d039aa9f33f9783c8349
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2020-12-30 00:39:13 +0100

    Changes

--- src/main/java/Contact.java
+++ src/main/java/Contact.java
@@ -34,16 +34,20 @@ public class Contact {
     }
 
     public void validateFirstName() {
-        if (this.firstName == null)
-            throw new RuntimeException("First Name Cannot be null");
+        if (this.firstName.isBlank())
+            throw new RuntimeException("First Name Cannot be null or empty");
     }
 
     public void validateLastName() {
-        if (this.lastName == null)
-            throw new RuntimeException("Last Name Cannot be null");
+        if (this.lastName.isBlank())
+            throw new RuntimeException("Last Name Cannot be null or empty");
     }
 
     public void validatePhoneNumber() {
+        if (this.phoneNumber.isBlank()){
+            throw new RuntimeException("Phone Name Cannot be null or empty");
+        }
+
         if (this.phoneNumber.length() != 10) {
             throw new RuntimeException("Phone Number Should be 10 Digits Long");
         }
--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,17 +1,30 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ContactManagerTest {
 
     private ContactManager contactManager;
 
+    @BeforeAll
+    public static void setupAll() {
+        System.out.println("Should Print Before All Tests");
+    }
+
     @BeforeEach
     public void setup() {
+        System.out.println("Instantiating Contact Manager");
         contactManager = new ContactManager();
     }
 
@@ -20,33 +33,130 @@ public class ContactManagerTest {
     public void shouldCreateContact() {
         contactManager.addContact("John", "Doe", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
     }
 
     @Test
     @DisplayName("Should Not Create Contact When First Name is Null")
-    public void shouldNotCreateContactWhenFirstNameIsNull() {
-        assertThrows(RuntimeException.class,
-                () -> contactManager.addContact(null,
-                        "Doe",
-                        "0123456789"));
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Doe", "0123456789");
+        });
     }
 
     @Test
     @DisplayName("Should Not Create Contact When Last Name is Null")
-    public void shouldNotCreateContactWhenLastNameIsNull() {
-        assertThrows(RuntimeException.class,
-                () -> contactManager.addContact("John",
-                        null,
-                        "0123456789"));
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", null, "0123456789");
+        });
     }
 
     @Test
     @DisplayName("Should Not Create Contact When Phone Number is Null")
-    public void shouldNotCreateContactWhenPhoneNumberIsNull() {
-        ContactManager contactManager = new ContactManager();
-        assertThrows(RuntimeException.class,
-                () -> contactManager.addContact("John",
-                        "Doe",
-                        null));
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", "Doe", null);
+        });
+    }
+
+    @Test
+    @DisplayName("Should Create Contact")
+    @EnabledOnOs(value = OS.MAC, disabledReason = "Should Run only on MAC")
+    public void shouldCreateContactOnMAC() {
+        contactManager.addContact("John", "Doe", "0123456789");
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    @Test
+    @DisplayName("Test Contact Creation on Developer Machine")
+    public void shouldTestContactCreationOnDEV() {
+        Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")));
+        contactManager.addContact("John", "Doe", "0123456789");
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    @Test
+    @DisplayName("Phone Number should start with 0")
+    public void shouldTestPhoneNumberFormat() {
+        contactManager.addContact("John", "Doe", "0123456789");
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+
+    @Nested
+    class RepeatedTests {
+        @DisplayName("Repeat Contact Creation Test 5 Times")
+        @RepeatedTest(value = 5,
+                name = "Repeating Contact Creation Test {currentRepetition} of {totalRepetitions}")
+        public void shouldTestContactCreationRepeatedly() {
+            contactManager.addContact("John", "Doe", "0123456789");
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+    }
+
+    @Nested
+    class ParameterizedTests {
+        @DisplayName("Phone Number should match the required Format")
+        @ParameterizedTest
+        @ValueSource(strings = {"0123456789", "0123456798", "0123456897"})
+        public void shouldTestPhoneNumberFormatUsingValueSource(String phoneNumber) {
+            contactManager.addContact("John", "Doe", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("CSV Source Case - Phone Number should match the required Format")
+        @ParameterizedTest
+        @CsvSource({"0123456789", "0123456798", "0123456897"})
+        public void shouldTestPhoneNumberFormatUsingCSVSource(String phoneNumber) {
+            contactManager.addContact("John", "Doe", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("CSV File Source Case - Phone Number should match the required Format")
+        @ParameterizedTest
+        @CsvFileSource(resources = "/data.csv")
+        public void shouldTestPhoneNumberFormatUsingCSVFileSource(String phoneNumber) {
+            contactManager.addContact("John", "Doe", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+    }
+
+    @DisplayName("Method Source Case - Phone Number should match the required Format")
+    @ParameterizedTest
+    @MethodSource("phoneNumberList")
+    public void shouldTestPhoneNumberFormatUsingMethodSource(String phoneNumber) {
+        contactManager.addContact("John", "Doe", phoneNumber);
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    private static List<String> phoneNumberList() {
+        return Arrays.asList("0123456789", "0123456798", "0123456897");
+    }
+
+    @Test
+    @DisplayName("Test Should Be Disabled")
+    @Disabled
+    public void shouldBeDisabled() {
+        throw new RuntimeException("Test Should Not be executed");
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Should Execute After Each Test");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        System.out.println("Should be executed at the end of the Test");
     }
 }
